9f34c6718bee580c77fe0972b487e927621b7769
Yeison Garzón T <toxiicsa@hotmail.com>
2022-10-13 20:31:18 -0500

    mvp

--- src/App.jsx
+++ src/App.jsx
@@ -14,25 +14,28 @@ import { cards, quotes } from "./constants";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import QuoteComponent from "./components/quote/quote";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import "./App.css";
 
+const wppUrl =
+  "https://api.whatsapp.com/send/?phone=+573177837117&text=Hola Maestro";
+const mapUrl =
+  "https://www.google.com/maps/@6.251144,-75.5729266,3a,90y,248.34h,83.93t/data=!3m6!1e1!3m4!1s4HQi6cdDhvwk3pwxFtAZmQ!2e0!7i16384!8i8192";
+
 function App() {
+  const [expanded, setExpanded] = useState(false);
+  const [toBottom, setToBottom] = useState(true);
+
   const topRef = useRef(null);
   const servicesRef = useRef(null);
   const quotesRef = useRef(null);
   const contactRef = useRef(null);
   const conditionsRef = useRef(null);
-
-  const [toBottom, setToBottom] = useState(true);
-
-  const handleScroll = () => {
-    const position = window.pageYOffset;
-    if (position == 0) {
-      setToBottom(true);
-    } else {
-      setToBottom(false);
-    }
-  };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -42,11 +45,8 @@ function App() {
     };
   }, []);
 
-  const handleClick = () => {
-    window.open(
-      "https://api.whatsapp.com/send/?phone=+573177837117&text=Hola Maestro",
-      "_blank"
-    );
+  const handleClick = (url) => {
+    window.open(url, "_blank");
   };
 
   const executeScroll = () => {
@@ -79,6 +79,19 @@ function App() {
     }
   };
 
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    if (position == 0) {
+      setToBottom(true);
+    } else {
+      setToBottom(false);
+    }
+  };
+
   return (
     <div id="main">
       <ResponsiveAppBarComponent scrollSection={scrollSection} />
@@ -92,7 +105,7 @@ function App() {
             <h2 style={{ fontSize: "3rem" }}>Amarres y brujería real</h2>
             <h3>Nosotros te escuchamos, agenda una asesoría </h3>
             <CustomButtonComponent
-              onClick={handleClick}
+              onClick={() => handleClick(wppUrl)}
               variant="outlined"
               style={{ padding: "6px 30px", marginTop: "40px", zIndex: "1" }}
             >
@@ -134,7 +147,7 @@ function App() {
           </Box>
           <div className="center">
             <CustomButtonComponent
-              onClick={handleClick}
+              onClick={() => handleClick(wppUrl)}
               variant="outlined"
               style={{ padding: "6px 30px", zIndex: "1", marginTop: "10vh" }}
             >
@@ -172,18 +185,162 @@ function App() {
       </SectionComponent>
 
       {/* Conctact */}
-      <SectionComponent
-        reference={contactRef}
-        backgroundColor="gray"
-        height={100}
-      ></SectionComponent>
+      <SectionComponent reference={contactRef} height={100}>
+        <div className="inital-section">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!4v1665707148259!6m8!1m7!1s4HQi6cdDhvwk3pwxFtAZmQ!2m2!1d6.251144018441813!2d-75.57292663306193!3f252.81936118531772!4f-4.576468756499068!5f0.7820865974627469"
+            width="600"
+            height="450"
+            style={{ border: 0, marginLeft: "40px" }}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+          <div>
+            <div style={{ margin: "0 40px" }}>
+              <h2 style={{ fontSize: "3rem" }}>Contacta con los Maestros</h2>
+              <h3>Revisa nuestra ubicación</h3>
+              <CustomButtonComponent
+                onClick={() => handleClick(mapUrl)}
+                variant="outlined"
+                style={{ padding: "6px 30px", marginTop: "40px", zIndex: "1" }}
+                startIcon={<LocationOnIcon />}
+              >
+                Abrir mapa
+              </CustomButtonComponent>
+            </div>
+          </div>
+        </div>
+      </SectionComponent>
 
       {/* Conditions */}
-      <SectionComponent
-        reference={conditionsRef}
-        backgroundColor="red"
-        height={100}
-      ></SectionComponent>
+      <SectionComponent reference={conditionsRef} height={100}>
+        <h2 style={{ padding: "65px 0", width: "100%", textAlign: "center" }}>
+          Terminos y condiciones
+        </h2>
+        <div style={{ margin: "0 auto", maxWidth: "75vw" }}>
+          <Accordion
+            expanded={expanded === "panel1"}
+            onChange={handleChange("panel1")}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                Condiciones del servicio
+              </Typography>
+              <Typography sx={{ color: "text.secondary" }}>
+                Establecen la forma en que se puede utilizar el servicio o
+                contenido.
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+                feugiat. Aliquam eget maximus est, id dignissim quam.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion
+            expanded={expanded === "panel2"}
+            onChange={handleChange("panel2")}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2bh-content"
+              id="panel2bh-header"
+            >
+              <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                Políticas de cookies
+              </Typography>
+              <Typography sx={{ color: "text.secondary" }}>
+                Capacidad de recordar su visita cuando vuelva a navegar por esta
+                página.
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Donec placerat, lectus sed mattis semper, neque lectus feugiat
+                lectus, varius pulvinar diam eros in elit. Pellentesque
+                convallis laoreet laoreet.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion
+            expanded={expanded === "panel3"}
+            onChange={handleChange("panel3")}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3bh-content"
+              id="panel3bh-header"
+            >
+              <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                ¿Qué información recopilamos?
+              </Typography>
+              <Typography sx={{ color: "text.secondary" }}>
+                La información sobre ti depende de la forma en la que usas
+                nuestros Productos
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
+                Integer sit amet egestas eros, vitae egestas augue. Duis vel est
+                augue.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion
+            expanded={expanded === "panel4"}
+            onChange={handleChange("panel4")}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel4bh-content"
+              id="panel4bh-header"
+            >
+              <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                ¿Cómo usamos tu información?
+              </Typography>
+              <Typography sx={{ color: "text.secondary" }}>
+                Usamos la información que recopilamos para ofrecerte una
+                experiencia personalizada, incluidos anuncios, además de los
+                otros fines que se detallan más adelante.
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl.
+                Integer sit amet egestas eros, vitae egestas augue. Duis vel est
+                augue.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion
+            expanded={expanded === "panel5"}
+            onChange={handleChange("panel5")}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel5bh-content"
+              id="panel5bh-header"
+            >
+              <Typography sx={{ width: "33%", flexShrink: 0 }}>
+                ¿Qué es la Política de privacidad y qué cubre?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                En la Política de privacidad, explicamos cómo recopilamos,
+                usamos, compartimos, retenemos y transferimos información.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+      </SectionComponent>
 
       <IconButton
         onClick={executeScroll}
@@ -202,7 +359,7 @@ function App() {
         statusMessage="Te responderé lo mas pronto posible..."
         avatar={AvatarWhatsapp}
       />
-      <FooterComponent />
+      <FooterComponent scrollSection={scrollSection} />
     </div>
   );
 }
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Button from "@mui/material/Button";
 import "./footer.css";
 
-const FooterComponent = () => {
+const FooterComponent = ({ scrollSection }) => {
   return (
-    <div id="footer">
+    <div id="footer" style={{ margin: "0 60px"}}>
       <Grid container spacing={2} columns={15}>
         <Grid item xs={5} className="center">
           <span>Copyright©2022</span>
         </Grid>
         <Grid item xs={5} className="center">
-          <a href="#">
+          <Link href="#" underline="none" style={{ color: "white" }}>
             <span>Contacto del desarrollador</span>
-          </a>
+          </Link>
         </Grid>
         <Grid item xs={5} className="center">
-          <span>Terminos y condiciones</span>
+          <Button onClick={() => scrollSection(4)}  sx={{ color: "white", display: "block" }}>
+            <span>Terminos y condiciones</span>
+          </Button>
         </Grid>
       </Grid>
     </div>
